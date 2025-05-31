5629075f8a70d48a81521efb9af65f785d43b32f
sufian06 <sufian4se@gmail.com>
2025-05-31 14:59:35 +0600

    fix an issue

--- src/components/ui/header/Navbar.js
+++ src/components/ui/header/Navbar.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <div className="relative">
+    <div className="relative hidden lg:block">
       <div className="mb-4 shadow-2xs sticky left-0 top-0 z-40">
         <nav className="flex justify-between items-center py-6 px-12">
           <div>
