8389cba1a2bd834d04757128281dc0951f165214
sufian06 <sufian4se@gmail.com>
2025-06-06 16:56:23 +0600

    clerk sign-in, sign-up added

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -36,16 +36,7 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
+                <Link href={"/"}>Logout</Link>
               </li>
             </ul>
           </div>
